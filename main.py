b67037c94dc0d313cee7dcd5230963ad40c75f1f
0fill <fikrosek@seznam.cz>
2024-06-17 10:48:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-text1 = open("data/text1.txt","r+")
-text2 = open("data/text2.txt","r+")
-text1.write("Suppose end get boy warrant general natural. Delightful met sufficient projection ask.\nDecisively everything principles if preference do impression of. Preserved oh so difficult")
-text1.close()
-text2.write("Suppose end get boy warrant general natural. Delightful met sufficient projection ask.\nrepulsive on in household. In what do miss time be. Valley as be appear cannot so by.")
-text2.close()
+
 
